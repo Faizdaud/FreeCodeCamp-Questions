e8738db3bf95307d935387325bbf818b0fb6426a
Faizdaud <ahmadfaizdaud007@gmail.com>
2022-04-21 15:14:10 +0800

    saya dah buat free code camp question

---
+++ factorial.js
@@ -0,0 +1,13 @@
+function factorialize(num) {
+    if(i=== 0 || i === 1){
+      return 1;
+    }
+  
+    for(var i = num-1; i >= 1; i-- ){
+      num = num * i;
+    }
+    return num;
+  }
+  
+  factorialize(5);
+  console.log(factorialize(5));
